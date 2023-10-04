849d56a08183d2e487e5b14543c7c152ca1e2aa4
Koima-Sam <sam17koima@gmail.com>
2023-10-04 16:03:15 +0300

    Fix the search issue

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from './search.svg'
 import "./App.css";
 import MovieCard from "./MovieCard";
-const apiUrl = 'https://www.omdbapi.com/?apikey=b6003d8a&s=Meg';
 
-// Then, use `apiUrl` in your fetch or XMLHttpRequest request
-
+const apiUrl = 'https://www.omdbapi.com/?apikey=b6003d8a&s=';
 
 function App() {
-  const [movies,setMovies] = useState([])
-  const [searchTerm, setSearchTerm] = useState("")
-  // 2cedc0e5
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("Spiderman");
+
   const searchMovies = async (title) => {
-    const response = await fetch(`${apiUrl}&s=${title}`);
+    const response = await fetch(`${apiUrl}${title}`);
     const data = await response.json();
-    setMovies(data.Search);
+    setMovies(data.Search || []);
   };
-  // Fetch the movie items
+
   useEffect(() => {
-    searchMovies("Meg");
-  }, []);
+    // Fetch the movie items when searchTerm changes
+    searchMovies(searchTerm);
+  }, [searchTerm]); // Include searchTerm as a dependency
+
   return (
     <div className="app">
       <h1>MovieHub</h1>
 
       <div className="search">
-        <input type="text" 
-        placeholder="Search for Movies" 
-        value={searchTerm} 
-        onChange={(e) =>{setSearchTerm(e.target.value)}}
+        <input
+          type="text"
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={Search} alt="search icon" 
-        onClick={()=> searchMovies(searchTerm)}
-        
+        <img
+          src={Search}
+          alt="search icon"
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
-      {/* Movies list  and handle searches that are empty*/}
+
+      {/* Movies list and handle searches that are empty */}
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
@@ -47,7 +50,6 @@ function App() {
           <h2>No movies found</h2>
         </div>
       )}
-      
     </div>
   );
 }
